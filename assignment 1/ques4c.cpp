6462e42e1523f87160829de4959584a0aa0a5ae6
kanika190905 <sahnikanika450@gmail.com>
2025-08-18 13:56:06 +0530

    update

--- assignment 1/ques4c.cpp
+++ assignment 1/ques4c.cpp
@@ -1,40 +1,51 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-void input(int mat[100][100], int rows, int cols) {
-    cout << "enter elements:\n";
-    for(int i = 0; i < rows; i++) {
-        for(int j = 0; j < cols; j++) {
+void input(int mat[100][100], int n) {
+    cout << "enter matrix elements:\n";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
             cin >> mat[i][j];
         }
     }
 }
-void transpose(int mat[100][100], int trans[100][100], int rows, int cols) {
-    for(int i = 0; i < rows; i++) {
-        for(int j = 0; j < cols; j++) {
-            trans[j][i] = mat[i][j];
-        }
-    }
-}
-void display(int mat[100][100], int rows, int cols) {
-    cout << "matrix\n";
-    for(int i = 0; i < rows; i++) {
-        for(int j = 0; j < cols; j++) {
+
+void output(int mat[100][100], int n) {
+    cout << "matrix:\n";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
             cout << mat[i][j] << " ";
         }
         cout << endl;
     }
 }
+
+void transpose(int mat[100][100], int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            int temp = mat[i][j];
+            mat[i][j] = mat[j][i];
+            mat[j][i] = temp;
+        }
+    }
+}
+
 int main() {
-    int mat[100][100], trans[100][100];
-    int rows, cols;
-	cout << "enter number of rows and columns  ";
-    cin >> rows >> cols;
-	input(mat, rows, cols);
-    transpose(mat, trans, rows, cols);
-    cout << "\ntransposed matrix:\n";
-    display(trans, cols, rows); 
+    int n;
+    cout << "size of square matrix: ";
+    cin >> n;
+
+    int mat[100][100];
+
+    input(mat, n);
+
+    cout << "\noriginal Matrix:\n";
+    output(mat, n);
+
+    transpose(mat, n);
+
+    cout << "\ntransposed Matrix:\n";
+    output(mat, n);
 
     return 0;
 }
-
